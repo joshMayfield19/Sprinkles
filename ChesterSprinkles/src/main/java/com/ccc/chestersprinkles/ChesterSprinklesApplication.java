32b3f70841e5e20ae4b168cc6d3f372b55e6d4bf
Caleb Hall <caleb.a.hall@gmail.com>
2018-06-03 19:44:51 -0500

    Added SQLite to pom and began build.sql script.

--- ChesterSprinkles/src/main/java/com/ccc/chestersprinkles/ChesterSprinklesApplication.java
+++ ChesterSprinkles/src/main/java/com/ccc/chestersprinkles/ChesterSprinklesApplication.java
@@ -1,8 +1,12 @@
 package com.ccc.chestersprinkles;
 
+import java.sql.DriverManager;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.sqlite.JDBC;
+import org.sqlite.SQLiteJDBCLoader;
 
 import com.ccc.chestersprinkles.carnivaltours.service.CTAdventureSessionService;
 import com.ccc.chestersprinkles.service.SlackUserService;
@@ -10,8 +14,14 @@ import com.ccc.chestersprinkles.service.SlackUserService;
 @SpringBootApplication(scanBasePackages = {"me.ramswaroop.jbot", "com.ccc.chestersprinkles"})
 public class ChesterSprinklesApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		initializeSqlite();
 		SpringApplication.run(ChesterSprinklesApplication.class, args);
+	}
+	
+	private static void initializeSqlite() throws Exception {
+		SQLiteJDBCLoader.initialize();
+		DriverManager.registerDriver(new JDBC());
 	}
 	
 	@Bean
---
+++ ChesterSprinkles/src/main/resources/build.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `users` (`user_id` INTEGER PRIMARY KEY AUTOINCREMENT, `first_name` TEXT NOT NULL, `last_name` TEXT NOT NULL, `slack_id` TEXT NOT NULL);
+
+CREATE TABLE `challenges` (`challenge_id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT NOT NULL, `date_announced` TEXT NOT NULL, `presentation_date` TEXT NOT NULL);
+
+CREATE TABLE `presentations` (`presentation_id` INTEGER PRIMARY KEY AUTOINCREMENT, `submitter_user_id` INTEGER NOT NULL, `challenge_id` INTEGER NOT NULL, FOREIGN KEY(`submitter_user_id`) REFERENCES `users`(`user_id`), FOREIGN KEY(`challenge_id`) REFERENCES `challenges`(`challenge_id`));
+
+CREATE TABLE `pirate_ships` (`ship_id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT NOT NULL, `captain_user_id` INTEGER NOT NULL); --No clue what the difference is between ship points and overall ship points 
+
+CREATE TABLE `pirates` (`pirate_id` INTEGER PRIMARY KEY AUTOINCREMENT, `user_id` INTEGER NOT NULL, `ship_id` INTEGER NOT NULL, `points` INTEGER, `doubloons` INTEGER, FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`), FOREIGN KEY(`ship_id`) REFERENCES `pirate_ships`(`ship_id`));
+
+CREATE TABLE `challenge_ideas` (`ch_idea_id` INTEGER PRIMARY KEY AUTOINCREMENT, `user_id` TEXT NOT NULL, `description` TEXT NOT NULL, FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`));
+
+CREATE TABLE `command_ideas` (`co_idea_id` INTEGER PRIMARY KEY AUTOINCREMENT, `user_id` TEXT NOT NULL, `command` TEXT NOT NULL, `description` TEXT NOT NULL, FOREIGN KEY(`user_id`) REFERENCES `users`(`user_id`));
+
+CREATE TABLE `parrot_phrases` (`phrase_id` INTEGER PRIMARY KEY AUTOINCREMENT, `phrase` TEXT NOT NULL);
+
